444ae8decc87051ff69177549b63eac895e9edb3
gtatman <gabe31430@gmail.com>
2023-06-30 14:23:06 -0500

    KING-108 Updating Gabe to JOYN

--- src/main_gabe.py
+++ src/main_gabe.py
@@ -3,7 +3,7 @@
 # Date: 06/22/23
 ##
 
-from kingscripts.operations import greasebook
+from kingscripts.operations import greasebook, joyn, combocurve
 from kingscripts.analytics import enverus
 
 # Python Packages
@@ -12,6 +12,7 @@ from combocurve_api_v1 import ServiceAccount
 import os
 import datetime as dt
 from datetime import timedelta
+import pandas as pd
 
 # load .env file
 load_dotenv()
@@ -19,6 +20,8 @@ load_dotenv()
 # getting API Key
 enverusApiKey = os.getenv('ENVERUS_API')
 greasebookApi = os.getenv('GREASEBOOK_API_KEY')
+joynUsername = str(os.getenv('JOYN_USERNAME'))
+joynPassword = str(os.getenv('JOYN_PASSWORD'))
 
 # Working Directories
 # workingDirectoryData = os.getenv("WORKING_DIRECTORY_DATA") ## Backup file
@@ -61,6 +64,10 @@ irvinsisters53m1h = "irvinsisters53m1h"
 pshigoda752h = "pshigoda752h"
 itSqlTable = "itSpend"
 
+# Working Zone
+
+# End Working Zone
+
 # Gets Browning 518H Production Data
 browing518HProductionMonthtlyData = enverus.getWellProductionData(
     apiKey=enverusApiKey,
@@ -75,14 +82,9 @@ NaughtyList = greasebook.getBatteryProductionData(
     greasebookApi=greasebookApi
 )
 
-totalAssetProduction = NaughtyList[1]
-totalAssetProduction.to_csv(
-    r"C:\Users\gtatman\OneDrive - King Operating\KOC Datawarehouse\totalAssetProduction.csv", index=False)
-
-# greasebookComments = greasebook.getComments(
-#     workingDataDirectory=kocDatawarehouse,
-#     greasebookApi=greasebookApi
-# )
+# totalAssetProduction = NaughtyList[1]
+# totalAssetProduction.to_csv(
+#     r"C:\Users\gtatman\OneDrive - King Operating\KOC Datawarehouse\totalAssetProduction.csv", index=False)
 
 allocatedProductionData = greasebook.allocateWells(
     days=30,
@@ -92,19 +94,47 @@ allocatedProductionData = greasebook.allocateWells(
     edgeCaseRollingAverage=7
 )
 
-# # Backup
-# allocatedProductionData.to_json(
-#     workingDirectoryData + r"\comboCurveAllocatedProduction.json", orient="records")
-# allocatedProductionData.to_csv(
-#     workingDirectoryData + r"\comboCurveAllocatedProduction.csv", index=False)
-
 # KOC Datawarehouse LIVE DUMP
-allocatedProductionData.to_csv(
-    kocDatawarehouse + r"\production\comboCurveAllocatedProduction.csv", index=False)
+print("Begin Exporting Greasebook Allocated Production Data to KOC Datawarehouse...")
+allocatedProductionData.to_excel(
+    kocDatawarehouse + r"\production\comboCurveAllocatedProduction.xlsx", index=False)
 allocatedProductionData.to_json(
     kocDatawarehouse + r"\production\comboCurveAllocatedProduction.json", orient="records")
-allocatedProductionData.to_csv(
-    kocDatawarehouse + r"\production\allocationVersionControl\comboCurveAllocatedProduction_" + yesDateString + ".csv", index=False)
+print("Finished Exporting Greasebook Allocated Production Data to KOC Datawarehouse!")
+
+# JOYN STACK
+# DAILY ALLOCATED PRODUCTION
+joynData = joyn.getDailyAllocatedProduction(
+    workingDataDirectory=kocDatawarehouse,
+    joynUsername=joynUsername,
+    joynPassword=joynPassword,
+)
+
+print("Begin Exporting Master Joyn Data to KOC Datawarehouse...")
+joynData.to_excel(kocDatawarehouse +
+                  r"\production\masterJoynData.xlsx", index=False)
+print("Finished Exporting Master Joyn Data to KOC Datawarehouse!")
+
+# Master Greasebook Data
+print("Reading Master Greasebook Data...")
+masterGreasebookData = pd.read_excel(os.getenv("MASTER_GREASEBOOK_DATA"))
+print("Finished Reading Master Greasebook Data!")
+
+# MERGE JOYN DATA WITH MASTER GREASEBOOK DATA
+masterData = joyn.mergeBIntoA(
+    A=masterGreasebookData,
+    B=joynData
+)
+
+# EXPORT MASTER DATA TO KOC DATAWAREHOUSE
+print("Begin Exporting Master Data to KOC Datawarehouse...")
+masterData.to_excel(
+    kocDatawarehouse + r"\production\masterAllocatedProductionData.xlsx", index=False)
+
+masterData.to_json(kocDatawarehouse +
+                   r"\production\masterAllocatedProductionData.json", orient="records")
+print("Finished Exporting Master Data to KOC Datawarehouse!")
 
 
+print("Yay!")
 x = 5
