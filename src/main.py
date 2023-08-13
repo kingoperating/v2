5dc78376ff6eb1bb2600faff73c3a5523e3a2f47
Michael Tanner <m.tanner161@gmail.com>
2023-08-13 13:14:37 -0500

    updating lookback JOYN to 3 days

--- src/main.py
+++ src/main.py
@@ -146,7 +146,7 @@ joynData = joyn.getDailyAllocatedProduction(
     workingDataDirectory=kocDatawarehouse,
     joynUsername=joynUsername,
     joynPassword=joynPassword,
-    daysToLookBack=1
+    daysToLookBack=3
 )
 
 print("Begin Exporting Master Joyn Data to KOC Datawarehouse...")
