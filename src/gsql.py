0790e46fd5bac9d03a9119a7d4c40413ec2ddcd7
gtatman <gabe31430@gmail.com>
2024-01-24 08:29:33 -0600

    Made some changes to the headers of the SQL Server (added AssetID, removed comments and wellheader data)

--- src/gsql.py
+++ src/gsql.py
@@ -19,6 +19,11 @@ import pandas as pd
 import numpy as np
 from dotenv import load_dotenv
 from combocurve_api_v1 import ServiceAccount
+import time
+
+
+start_time = time.time()
+
 
 # load .env file
 load_dotenv()
@@ -40,12 +45,50 @@ getWellHeaderData = joyn.getWellHeaderData(
     joynPassword=joynPassword
 )
 
+# # Post header data to server
+# tech.putDataReplace(
+#     server=kingLiveServer,
+#     database = "wells",
+#     data = getWellHeaderData,
+#     tableName= "header_data"
+# )
+
+# dailyProduction = joyn.getDailyAllocatedProductionRaw(
+#     joynUsername=joynUsername,
+#     joynPassword=joynPassword,
+#     wellHeaderData=getWellHeaderData,
+#     daysToLookBack = 365
+# )
+
+# # Post daily production to server
+
+# tech.putDataReplace(
+#     server=kingLiveServer,
+#     database = "production",
+#     data = dailyProduction,
+#     tableName= "daily_production_2"
+# )
+
+# print("Daily Production Posted to SQL Server")
+
+# end_time = time.time()
+
+# elapsed_time_seconds = end_time - start_time
+
+# # Convert seconds to minutes with two decimal places
+# elapsed_time_minutes = elapsed_time_seconds / 60
+
+# # Print the runtime in minutes with two decimal places
+# print(f"Script execution time: {elapsed_time_minutes:.2f} minutes")
+
+# x = 5
+
 # get Daily Well Reading
 DailyWellReading = joyn.getDailyWellReading(
     joynUsername=joynUsername,
     joynPassword=joynPassword,
     wellHeaderData=getWellHeaderData,
-    daysToLookBack = 5
+    daysToLookBack = 365
 )
 
 # Post Daily Well Reading to SQL Server
@@ -57,65 +100,58 @@ tech.putDataReplace(
 )
 
 
-x = 5
 
 
 
 
 
-forecast = combocurve.getDailyForecastVolume(
-    projectIdKey="6572267d2bcb2950186aca90",
-    forecastIdKey="65722aa4ea66fa10d79f2be6",
-    serviceAccount=serviceAccount,
-    comboCurveApi = comboCurveApiKey
-)
 
-print(len(forecast))
-
-print("Writing forecast to SQL Server...")
-
-tech.putDataReplace(
-     server=kingLiveServer,
-     database = "production",
-     data = forecast,
-     tableName= "daily_forecast"
-
-)
-
-x = 5
-# How to query
-
-# data = tech.getData(
-#     serverName=kingLiveServer,
-#     databaseName=kingProductionDatabase,
-#     tableName="prod_bad_pumper_data"
+# forecast = combocurve.getDailyForecastVolume(
+#     projectIdKey="6572267d2bcb2950186aca90",
+#     forecastIdKey="65722aa4ea66fa10d79f2be6",
+#     serviceAccount=serviceAccount,
+#     comboCurveApi = comboCurveApiKey
 # )
 
-# How to write 
+# print(len(forecast))
 
-# king_gabe_table = str(os.getenv('SQL_GABE_DATABASE'))
-# tech.putData(
-#     server=kingLiveServer,
-#     database = king_gabe_table,
-#     data = data,
-#     tableName= "test_table"
+# print("Writing forecast to SQL Server...")
+
+# tech.putDataReplace(
+#      server=kingLiveServer,
+#      database = "production",
+#      data = forecast,
+#      tableName= "daily_forecast"
+
 # )
-# print("Hooray")
 
+# x = 5
+# # How to query
 
-# ** WORKING SPACE **
+# # data = tech.getData(
+# #     serverName=kingLiveServer,
+# #     databaseName=kingProductionDatabase,
+# #     tableName="prod_bad_pumper_data"
+# # )
 
+# # How to write 
 
-## Get JOYN Username and password
-joynUsername = str(os.getenv('JOYN_USERNAME'))
-joynPassword = str(os.getenv('JOYN_PASSWORD'))
+# # king_gabe_table = str(os.getenv('SQL_GABE_DATABASE'))
+# # tech.putData(
+# #     server=kingLiveServer,
+# #     database = king_gabe_table,
+# #     data = data,
+# #     tableName= "test_table"
+# # )
+# # print("Hooray")
+
 
 ## Get the well header data that we want to pull from JOYN from main.py
 
-wellHeaderData = joyn.getWellHeaderData(
-    joynUsername=joynUsername,
-    joynPassword=joynPassword
-)
+# wellHeaderData = joyn.getWellHeaderData(
+#     joynUsername=joynUsername,
+#     joynPassword=joynPassword
+# )
 
 ## Connect to the SQL server and pull the latest master production table
 
@@ -140,3 +176,12 @@ wellHeaderData = joyn.getWellHeaderData(
 # )
 
 
+end_time = time.time()
+
+elapsed_time_seconds = end_time - start_time
+
+# Convert seconds to minutes with two decimal places
+elapsed_time_minutes = elapsed_time_seconds / 60
+
+# Print the runtime in minutes with two decimal places
+print(f"Script execution time: {elapsed_time_minutes:.2f} minutes")
