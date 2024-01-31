ed6bd68c44d7a5a51773b21d0f944dfbe5dd0cd0
Michael Tanner <m.tanner161@gmail.com>
2024-01-31 09:46:20 -0600

    DEV-178 adding etl_main.py

---
+++ src/etl_main.py
@@ -0,0 +1,34 @@
+##  This script runs the 3 main ETL scripts for the kingscripts
+##  includes etl_combocurve, etl_scada, and etl_joyn
+##
+##  Version 3.4.0
+##  Developer: Michael Tanner, Gabe Tatman
+
+import datetime as dt
+from kingscripts.analytics import king
+
+function = "etl_main"
+
+print("Starting etl_main")
+
+dateNow = dt.datetime.now()
+
+import etl_combocurve
+import etl_scada
+import etl_joyn
+
+## calculate time to run
+dateEnding = dt.datetime.today()
+
+runtimeSeconds = (dateEnding - dateNow).total_seconds()
+
+usageFunction = king.updateUsageStatsEtlRuntime(
+    etlStartTime=dateNow,
+    etlEndTime=dateEnding,
+    function=function,
+    runtime=runtimeSeconds
+)
+
+usageStat = king.updateUsageStatsEtl(dateNow)
+
+print("ETL Process Complete")
